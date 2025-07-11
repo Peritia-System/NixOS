135412439781d4da2bb2bc05c65f953b9bce9067
Peritia <Peritia@peritia.space>
2025-07-11 14:13:58 +0200

    fix

--- Misc/Nyx-Tools/nyx-tool.nix
+++ Misc/Nyx-Tools/nyx-tool.nix
@@ -2,7 +2,7 @@
 
 let
   cfg = config.modules.nix-tool;
-  scriptTargetPath = "${nixDirectory}/Misc/Nyx-Tools/zsh/nyx-cleanup.zsh";
+  scriptTargetPath = "${cfg.nixDirectory}/Misc/Nyx-Tools/zsh/nyx-cleanup.zsh";
 in
 {
   options.modules.nix-tool = {
